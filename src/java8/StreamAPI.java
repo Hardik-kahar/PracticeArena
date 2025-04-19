81ca1c94c34fac1a3c91fc283c11c1cd17b4cfa9
Hardik-kahar <kahardik1997@gmail.com>
2025-04-19 14:22:31 +0530

    add link

--- src/java8/StreamAPI.java
+++ src/java8/StreamAPI.java
@@ -2,8 +2,16 @@ package java8;
 
 public class StreamAPI {
 
+    /*
+        https://blog.devgenius.io/java-8-coding-and-programming-interview-questions-and-answers-62512c44f062
+
+        https://medium.com/@mehar.chand.cloud/java-stream-coding-interview-questions-part-1-dc39e3575727
+
+        https://medium.com/@mehar.chand.cloud/java-stream-coding-interview-questions-part-2-9f3aad0025f3
+
+        https://medium.com/@mehar.chand.cloud/java-stream-hard-interview-questions-54ea0de40acc
+     */
     public static void main(String[] args) {
 
-//        tgest
     }
 }
